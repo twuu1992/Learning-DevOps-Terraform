30231c8901335c84567a5f3e63cb0d163d2c1526
h3lloworld <astroneer976@gmail.com>
2023-02-17 11:43:53 +1100

    Add health check for target group 80 port

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -16,6 +16,9 @@ resource "aws_lb_target_group" "frontend" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    protocol = "HTTP"
+  }
 }
 
 resource "aws_lb_target_group" "backend" {
